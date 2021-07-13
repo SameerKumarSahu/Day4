67a5d2f08c3939c1e98e768fbb2f27aa40e13433
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-13 12:09:19 +0530

    Adding Part time wage

--- EmpWage.java
+++ EmpWage.java
@@ -1,38 +1,43 @@
 public class EmpWage {
 
-	int isFullTime;
+	int isFullTime, isPartTime;
 	double empCheck;
 	static int empRatePerHr;
-	static int empHrs;
+	static int empFullTimeHrs;
+	static int empPartTimeHrs;
 	static int empDailyWage ;
 
-
-	public EmpWage(int i, double c ,int r,int h)
+	public EmpWage(int f,int p, double c ,int r,int h1,int h2)
 	{
-		this.isFullTime=i;
+		this.isFullTime=f;
+		this.isPartTime=p;
 		this.empCheck=c;
 		this.empRatePerHr=r;
-		this.empHrs=h;
+		this.empFullTimeHrs=h1;
+		this.empPartTimeHrs=h2;
 	}
 
 	public static void main(String[] args) {
 
-	EmpWage s1= new EmpWage (1,Math.floor(Math.random()*10)%2,20,8);
+	EmpWage s1= new EmpWage (1,2,Math.floor(Math.random()*10)%3,20,8,4);
 
-		if (s1.isFullTime == s1.empCheck)
-		{
-
+		if (s1.isFullTime == s1.empCheck) {
 			System.out.println("Employee is Present");
-			empDailyWage = empRatePerHr * empHrs;
-			System.out.println("Employee Daily wage is "+empDailyWage);
+			 empDailyWage = empRatePerHr * empFullTimeHrs;
+			 System.out.println("Employee Fullday wage is "+empDailyWage);
+
+			}
+		else if (s1.isPartTime == s1.empCheck) {
+			System.out.println("Employee is Present");
+			 empDailyWage = empRatePerHr * empPartTimeHrs;
+			 System.out.println("Employee Part time wage is "+empDailyWage);
+
 		}
-
 		else {
-
 			System.out.println("Employee is Absent");
-		     }
+		}
 	}
 
-
-
 }
+
+
