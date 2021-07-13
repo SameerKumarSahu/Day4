615eeb4c896fb146f7d43eb54011be3c11bb693d
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-13 11:51:35 +0530

    Employee Daily Wage

--- EmpWage.java
+++ EmpWage.java
@@ -2,22 +2,30 @@ public class EmpWage {
 
 	int isFullTime;
 	double empCheck;
+	static int empRatePerHr;
+	static int empHrs;
+	static int empDailyWage ;
 
-	public EmpWage(int i, double s)
+
+	public EmpWage(int i, double c ,int r,int h)
 	{
 		this.isFullTime=i;
-		this.empCheck=s;
+		this.empCheck=c;
+		this.empRatePerHr=r;
+		this.empHrs=h;
 	}
-
 
 	public static void main(String[] args) {
 
-	EmpWage s1= new EmpWage (1,Math.floor(Math.random()*10)%2);
+	EmpWage s1= new EmpWage (1,Math.floor(Math.random()*10)%2,20,8);
 
-		if (s1.isFullTime == s1.empCheck) {
+		if (s1.isFullTime == s1.empCheck)
+		{
 
 			System.out.println("Employee is Present");
-			}
+			empDailyWage = empRatePerHr * empHrs;
+			System.out.println("Employee Daily wage is "+empDailyWage);
+		}
 
 		else {
 
@@ -25,5 +33,6 @@ public class EmpWage {
 		     }
 	}
 
-}
 
+
+}
