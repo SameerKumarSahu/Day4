29a668a614ab45b41f57b048552040d4408a7dfb
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-13 11:36:35 +0530

    Check Employee Availability

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,29 @@
 public class EmpWage {
 
+	int isFullTime;
+	double empCheck;
+
+	public EmpWage(int i, double s)
+	{
+		this.isFullTime=i;
+		this.empCheck=s;
+	}
+
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
+
+	EmpWage s1= new EmpWage (1,Math.floor(Math.random()*10)%2);
+
+		if (s1.isFullTime == s1.empCheck) {
+
+			System.out.println("Employee is Present");
+			}
+
+		else {
+
+			System.out.println("Employee is Absent");
+		     }
 	}
 
 }
+
