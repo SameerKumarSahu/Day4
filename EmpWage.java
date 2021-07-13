8d667c6ab216dfb25efc60ae3248bbae6157053f
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-07-13 13:13:09 +0530

    Employee Monthly Salary

--- EmpWage.java
+++ EmpWage.java
@@ -5,34 +5,51 @@ public class EmpWage {
 	static int isFullTime, isPartTime;
 	static int empRatePerHr;
 	static int empHrs;
+	static int numberOfWorkingDays;
 
-	public EmpWage(int f,int p,int r)
+	public EmpWage(int f,int p,int r,int n)
 	{
 		this.isFullTime=f;
 		this.isPartTime=p;
 		this.empRatePerHr=r;
+		this.numberOfWorkingDays=n;
 	}
 
 	public static void main(String[] args) {
 
 		Random num = new Random ();
-		int empCheck =num.nextInt(3);
-		EmpWage s1= new EmpWage (1,2,20);
-
-		switch (empCheck) {
+		int totalSalary=0;
+		int Fullday=0;
+		int Halfday=0;
+		int Absent=0;
+		EmpWage s1= new EmpWage (1,2,20,20);
+		for (int day =0; day<20; day++) 
+		{
+			int empCheck =num.nextInt(3);
+			switch (empCheck) 
+			{
 			case 1:
 				empHrs=8;
-				System.out.println("Employee is present for Full day..");
+				Fullday++;
 				break;
 			case 2:
 				empHrs=4;
-				System.out.println("Employee is present for half day..");
+				Halfday++;
 				break;
 			default:
 				empHrs=0;
-		}
+				Absent++;
+			}
 		int empWage=empHrs*empRatePerHr;
-		System.out.println("Employee Salary is "+empWage);
+		 totalSalary = totalSalary+ empWage;
+		}
+		System.out.println( "Number of days Absent is " + Absent );
+
+		System.out.println( "Number of Fulldays present is  " + Fullday );
+
+		System.out.println( "Number of Halfdays present is " +Halfday );
+
+		System.out.println("Employee's Monthly Salary is "+totalSalary);
 
 	}
 
